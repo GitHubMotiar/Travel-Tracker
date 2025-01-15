b43f6c79f8ebb3ca61bce3f957d5065640605f72
Motiar Rahaman <rehanrehman2013@gmail.com>
2025-01-15 18:59:08 +0530

    new changes

--- index.js
+++ index.js
@@ -9,40 +9,44 @@ const db = new pg.Client({
   host: "localhost",
   database: "alternate_storoage",
   password: "Superuser",
-  post: 5432
+  port: 5432
 })
 db.connect();
 
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(express.static("public"));
-
-
 async function visitedCountries() {
   const result = await db.query("SELECT country_code FROM visited_countries")
   let countries = [];
   result.rows.forEach((country) => {
     countries.push(country.country_code)
   })
+  console.log(result.rows)
   return countries;
 }
 
 app.get("/", async (req, res) => {
   const countries = await visitedCountries()
-
   res.render("index.ejs", {
     countries: countries,
     total: countries.length
   })
-  db.end();
 
 });
 
 app.post("/add", async (req, res) => {
-
-
+const input=req.body["country"];
+console.log(input)
+const result =await db.query("SELECT country_code FROM countries WHERE country_name = $1", [input])
+console.log(result.rows)
+if(result.rows.length !== 0){
+  const data= result.rows[0];
+  const countryCode=data.country_code;
+  await db.query("INSERT INTO visited_countries (country_code) VALUES ($1)", [countryCode,]);
+  res.redirect("/");
+}
 
 })
-
 
 app.listen(port, () => {
   console.log(`Server running on http://localhost:${port}`);
